63fd66670b65a718912a5320c9f9709f44f123e8
me-safwan-07 <mesafwan07@gmail.com>
2025-03-09 19:56:15 +0530

    add routing and lazy loading for Home page; implement TMDB API service for fetching shows

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import './App.css'
+import { lazy, Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
+
 import VideoModal from './components/VideoModal'
 import SideBar from "./components/SideBar"
 import Header from './components/Header'
+import ScrollToTop from './components/ScrollToTop'
+import { Loader } from './components/Loader'
+
+import "react-loading-skeleton/dist/skeleton.css";
+import "swiper/css";
+
+
+const Home = lazy(() => import("./pages/Home"));
+
 function App() {
   return (
     <>
       <VideoModal />
       <SideBar />
       <Header />
+      <main className="">
+        <ScrollToTop>
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Suspense>
+        </ScrollToTop>
+      </main>
     </>
   )
 }
