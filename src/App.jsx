2b46f847aba1bb32fe20ec8ed639bce89780efe2
me-safwan-07 <mesafwan07@gmail.com>
2025-03-10 17:42:53 +0530

    add lazy loading for Detail page; implement route for movie details view

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import "swiper/css";
 
 
 const Home = lazy(() => import("./pages/Home"));
+const Detail = lazy(() => import("./pages/Detail"));
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Suspense fallback={<Loader />}>
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path='/:category/:id' element={<Detail />} />
             </Routes>
           </Suspense>
         </ScrollToTop>
