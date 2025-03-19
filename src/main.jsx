f52b6f9aafceed5ed190233e50ae84a910fc6b7d
me-safwan-07 <mesafwan07@gmail.com>
2025-03-20 00:27:15 +0530

    refactor main.jsx to improve import order and organization

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
+import { domAnimation, LazyMotion } from 'framer-motion';
 import { ApiProvider } from '@reduxjs/toolkit/query/react';
-import ThemeProvider from './context/themeContext.jsx';
 
 import { tmdbApi } from "./services/TMDB.js";
-
-import './index.css'
-import App from './App.jsx'
 import GlobalContextProvider from './context/globalContext.jsx';
-import { domAnimation, LazyMotion } from 'framer-motion';
+import ThemeProvider from './context/themeContext.jsx';
+import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
