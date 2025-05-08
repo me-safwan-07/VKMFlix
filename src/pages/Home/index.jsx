9b765ffa99be68d142f47e17c58640b2af6adaf1
me-safwan-07 <mesafwan07@gmail.com>
2025-05-08 21:37:06 +0530

    refactor: comment out unused movie fetching logic in Home component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,7 +2,7 @@ import { sections } from "../../constants";
 import { maxWidth } from "../../style";
 import { cn } from "../../utils/helper";
 import { Hero } from "./components";
-import { useGetShowsQuery } from "../../services/TMDB";
+// import { useGetShowsQuery } from "../../services/TMDB";
 import Section from "../../components/Section";
 import { Loader } from "../../components/Loader";
 import Error from "../../components/Error";
@@ -10,11 +10,11 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "@clerk/clerk-react";
 
 const Home = () => {
-  const { data, isLoading, isError } = useGetShowsQuery({
-    category: "movie",
-    type: "popular",
-    page: 1,
-  });
+  // const { data, isLoading, isError } = useGetShowsQuery({
+  //   category: "movie",
+  //   type: "popular",
+  //   page: 1,
+  // });
   const navigate = useNavigate();
   const { isSignedIn } = useAuth();
 
@@ -26,19 +26,19 @@ const Home = () => {
     }
   };
 
-  if (isLoading) {
-    return <Loader />;
-  }
+  // if (isLoading) {
+  //   return <Loader />;
+  // }
 
-  if (isError) {
-    return <Error error="Unable to fetch the movies! " />;
-  }
+  // if (isError) {
+  //   return <Error error="Unable to fetch the movies! " />;
+  // }
 
-  const popularMovies = data?.results.slice(0, 5);
+  // const popularMovies = data?.results.slice(0, 5);
 
   return (
     <>
-      <Hero movies={popularMovies} onMovieClick={handleMovieClick} />
+      {/* <Hero movies={popularMovies} onMovieClick={handleMovieClick} /> */}
       <div className={cn(maxWidth, "lg:mt-12 md:mt-8 sm:mt-6 xs:mt-4 mt-2")}>
         {sections.map(({ title, category, type }) => (
           <Section 
